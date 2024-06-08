cadae9b8ef58925151c9e0aaa09dcd30d5427849
zeroqs <smthq@bk.ru>
2024-06-08 04:56:06 +0300

    feat: add search vacancies

--- src/pages/Vacancys.tsx
+++ src/pages/Vacancys.tsx
@@ -1,20 +1,45 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { useFetchVacancys } from '@/shared/hooks/useFetchVacancys'
+import { Input } from '@/shared/ui/input'
 import { LoadingSpinner } from '@/shared/ui/loading-spinner'
+import { useDebounce } from '@/shared/ui/multi-select'
 import { VacancyCard } from '@/shared/ui/vacancy-card'
 
 const Vacancys = () => {
-  const { vacancys, loading } = useFetchVacancys()
+  const [search, setSearch] = useState('')
 
-  if (loading)
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <LoadingSpinner />
-      </div>
-    )
+  const searchDebouncedValue = useDebounce(search, 1000)
+
+  const { vacancys, loading } = useFetchVacancys(searchDebouncedValue)
+
+  const isVacancyEmpty = vacancys.length === 0
+
   return (
     <div className="container p-4 animate-fade flex flex-col gap-4">
+      <div className="flex gap-4">
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Поиск"
+        />
+      </div>
+
+      {loading && (
+        <div className="flex h-screen items-center justify-center">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {isVacancyEmpty && search && (
+        <div className="flex items-center justify-center mt-20">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            🥺 Ничего не найдено 🥺
+          </h3>
+        </div>
+      )}
+
       {vacancys.map((vacancy) => (
         <Link key={vacancy.id} to={`/vacancy/${vacancy.id}`}>
           <VacancyCard key={vacancy.id} {...vacancy} />
--- src/shared/hooks/useFetchVacancys.tsx
+++ src/shared/hooks/useFetchVacancys.tsx
@@ -9,10 +9,27 @@ interface Vacancy {
   createdAt: string
 }
 
-export const useFetchVacancys = () => {
+export const useFetchVacancys = (searchQuery?: string) => {
+  const [vacancysSearch, setVacancysSearch] = useState<Vacancy[]>([])
+
   const [vacancys, setVacancys] = useState<Vacancy[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
+
+  const fetchSearchVacancys = async () => {
+    setLoading(true)
+    try {
+      const response = await baseApi.get(
+        `/vacancy/search?searchQuery=${searchQuery}`,
+      )
+      const data = response.data
+      setVacancysSearch(data)
+    } catch (error) {
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   const fetchVacancys = async () => {
     setLoading(true)
@@ -31,8 +48,12 @@ export const useFetchVacancys = () => {
     fetchVacancys()
   }, [])
 
+  useEffect(() => {
+    if (searchQuery) fetchSearchVacancys()
+  }, [searchQuery])
+
   return {
-    vacancys,
+    vacancys: searchQuery ? vacancysSearch : vacancys,
     loading,
     error,
   }
