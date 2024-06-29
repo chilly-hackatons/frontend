d729c2f41d272e34969167abe5cc63525e2ea333
zeroqs <smthq@bk.ru>
2024-06-29 13:32:56 +0300

    chore: install effector

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,24 @@
 const { configure, presets } = require('eslint-kit')
 
 module.exports = configure({
- allowDebug: process.env.NODE_ENV !== 'production',
+  allowDebug: process.env.NODE_ENV !== 'production',
 
- presets: [
+  presets: [
     presets.imports({
-    sort: {
-      newline: true,
-    },
-    alias: {
-      paths: {
-      '@': './src',
+      sort: {
+        newline: true,
       },
-    },
+      alias: {
+        paths: {
+          '@': './src',
+        },
+      },
     }),
 
     presets.node(),
     presets.prettier(),
     presets.typescript(),
     presets.react(),
+    presets.effector(),
   ],
-
-
-
 })
